ef349cfb3ee4406abf6e650d4751017db384ff46
Joao Vitor P. Moraes <jvlppm@gmail.com>
2014-02-04 21:15:12 -0200

    Created Results screen.

--- src/PowerOfLove/PowerOfLove/Activities/ResultsScreen.cs
+++ src/PowerOfLove/PowerOfLove/Activities/ResultsScreen.cs
@@ -1,5 +1,8 @@
 ﻿using Jv.Games.Xna.Async;
 using Microsoft.Xna.Framework;
+using MonoGameLib.GUI.Base;
+using MonoGameLib.GUI.Components;
+using PowerOfLove.Components;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,20 +12,50 @@ namespace PowerOfLove.Activities
 {
     class ResultsScreen : Activity
     {
+        GUI _gui;
+
         public ResultsScreen(Game game, int gamePlayResult)
             : base(game)
         {
+            _gui = new GUI
+            {
+                CreateMessage(gamePlayResult),
+                CreateBackButton(game)
+            };
+        }
 
+        Component CreateMessage(int gameResult)
+        {
+            return new Label("Sorry if we, from Curitiba, look antisocial some times.\n" +
+                             "We're just afraid to be turned into zombies.\n" +
+                             "\n" +
+                             "You zombified " + gameResult + " people.", "Fonts/DefaultFont")
+            {
+                Color = Color.White,
+                HorizontalOrigin = HorizontalAlign.Center,
+                VerticalOrigin = VerticalAlign.Middle,
+                Position = new Point(Game.GraphicsDevice.Viewport.Width / 2, Game.GraphicsDevice.Viewport.Height / 2 - 60)
+            };
+        }
+
+        Component CreateBackButton(Microsoft.Xna.Framework.Game game)
+        {
+            var btnBack = new Button(game, "Return") { HorizontalOrigin = HorizontalAlign.Center };
+            btnBack.Position = new Point(
+                Game.GraphicsDevice.Viewport.Width / 2 - btnBack.Size.X / 2,
+                Game.GraphicsDevice.Viewport.Height * 7 / 8);
+            btnBack.Clicked += (s, e) => Exit();
+            return btnBack;
         }
 
         protected override void Draw(Microsoft.Xna.Framework.GameTime gameTime)
         {
-            throw new NotImplementedException();
+            _gui.Draw(gameTime, SpriteBatch);
         }
 
         protected override void Update(Microsoft.Xna.Framework.GameTime gameTime)
         {
-            throw new NotImplementedException();
+            _gui.Update(gameTime);
         }
     }
 }
